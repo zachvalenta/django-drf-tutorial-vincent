84b3e2b9f1929e567d87cec876c0b5bd1cd253c3
zachvalenta <zjv5002@gmail.com>
2020-07-30 16:15:00 -0400

    add serializers and views

--- snippets/models.py
+++ snippets/models.py
@@ -1,4 +1,3 @@
-# snippets/models.py
 from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from rest_framework import generics
+from .models import Snippet
+from .serializers import SnippetSerializer
 
-# Create your views here.
+
+class IndexPageView(TemplateView):
+    template_name = 'index.html'
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
